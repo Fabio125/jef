2cabf3ed5715d69a4a06781b32fafc087962987e
Erwin <torres6593@outlook.com>
2019-06-13 09:16:31 -0500

    Setter Getter Producto

--- clases/Producto.php
+++ clases/Producto.php
@@ -30,105 +30,251 @@ class Producto {
         $this->idunidadbase=$idunidadbase;
         $this->total=$total;
 
-    }
-
-	public private get$Idproducto=Null() {
-		return this.$idproducto=null;
 	}
-
-	public void set$Idproducto=Null(private $idproducto=null) {
-		this.$idproducto=null = $idproducto=null;
-	}
-
-	public private get$Referencia=Null() {
-		return this.$referencia=null;
-	}
-
-	public void set$Referencia=Null(private $referencia=null) {
-		this.$referencia=null = $referencia=null;
-	}
-
-	public private get$Descripcion=Null() {
-		return this.$descripcion=null;
-	}
-
-	public void set$Descripcion=Null(private $descripcion=null) {
-		this.$descripcion=null = $descripcion=null;
-	}
-
-	public $idunidadmedida= getNull() {
-		return this.null;
-	}
-
-	public void setNull($idunidadmedida= null) {
-		this.null = null;
-	}
-
-	public private get$Siglaunidad=Null() {
-		return this.$siglaunidad=null;
-	}
-
-	public void set$Siglaunidad=Null(private $siglaunidad=null) {
-		this.$siglaunidad=null = $siglaunidad=null;
-	}
-
-	public private get$Descripcionunidad=Null() {
-		return this.$descripcionunidad=null;
-	}
-
-	public void set$Descripcionunidad=Null(private $descripcionunidad=null) {
-		this.$descripcionunidad=null = $descripcionunidad=null;
-	}
-
-	public private get$Precio=Null() {
-		return this.$precio=null;
-	}
-
-	public void set$Precio=Null(private $precio=null) {
-		this.$precio=null = $precio=null;
-	}
-
-	public private get$Cantidad=Null() {
-		return this.$cantidad=null;
-	}
-
-	public void set$Cantidad=Null(private $cantidad=null) {
-		this.$cantidad=null = $cantidad=null;
-	}
-
-	public private get$Tipoimpuesto=Null() {
-		return this.$tipoimpuesto=null;
-	}
-
-	public void set$Tipoimpuesto=Null(private $tipoimpuesto=null) {
-		this.$tipoimpuesto=null = $tipoimpuesto=null;
-	}
-
-	public private get$Tipoarticulo=Null() {
-		return this.$tipoarticulo=null;
-	}
-
-	public void set$Tipoarticulo=Null(private $tipoarticulo=null) {
-		this.$tipoarticulo=null = $tipoarticulo=null;
-	}
-
-	public private get$Idunidadbase=Null() {
-		return this.$idunidadbase=null;
-	}
-
-	public void set$Idunidadbase=Null(private $idunidadbase=null) {
-		this.$idunidadbase=null = $idunidadbase=null;
-	}
-
-	public private get$Total=Null() {
-		return this.$total=null;
-	}
-
-	public void set$Total=Null(private $total=null) {
-		this.$total=null = $total=null;
-	}
+	
+	
 
         
+
+    /**
+     * Get the value of idproducto
+     */ 
+    public function getIdproducto()
+    {
+        return $this->idproducto;
+    }
+
+    /**
+     * Set the value of idproducto
+     *
+     * @return  self
+     */ 
+    public function setIdproducto($idproducto)
+    {
+        $this->idproducto = $idproducto;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of referencia
+     */ 
+    public function getReferencia()
+    {
+        return $this->referencia;
+    }
+
+    /**
+     * Set the value of referencia
+     *
+     * @return  self
+     */ 
+    public function setReferencia($referencia)
+    {
+        $this->referencia = $referencia;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of descripcion
+     */ 
+    public function getDescripcion()
+    {
+        return $this->descripcion;
+    }
+
+    /**
+     * Set the value of descripcion
+     *
+     * @return  self
+     */ 
+    public function setDescripcion($descripcion)
+    {
+        $this->descripcion = $descripcion;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of idunidadmedida
+     */ 
+    public function getIdunidadmedida()
+    {
+        return $this->idunidadmedida;
+    }
+
+    /**
+     * Set the value of idunidadmedida
+     *
+     * @return  self
+     */ 
+    public function setIdunidadmedida($idunidadmedida)
+    {
+        $this->idunidadmedida = $idunidadmedida;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of siglaunidad
+     */ 
+    public function getSiglaunidad()
+    {
+        return $this->siglaunidad;
+    }
+
+    /**
+     * Set the value of siglaunidad
+     *
+     * @return  self
+     */ 
+    public function setSiglaunidad($siglaunidad)
+    {
+        $this->siglaunidad = $siglaunidad;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of descripcionunidad
+     */ 
+    public function getDescripcionunidad()
+    {
+        return $this->descripcionunidad;
+    }
+
+    /**
+     * Set the value of descripcionunidad
+     *
+     * @return  self
+     */ 
+    public function setDescripcionunidad($descripcionunidad)
+    {
+        $this->descripcionunidad = $descripcionunidad;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of precio
+     */ 
+    public function getPrecio()
+    {
+        return $this->precio;
+    }
+
+    /**
+     * Set the value of precio
+     *
+     * @return  self
+     */ 
+    public function setPrecio($precio)
+    {
+        $this->precio = $precio;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of cantidad
+     */ 
+    public function getCantidad()
+    {
+        return $this->cantidad;
+    }
+
+    /**
+     * Set the value of cantidad
+     *
+     * @return  self
+     */ 
+    public function setCantidad($cantidad)
+    {
+        $this->cantidad = $cantidad;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of tipoimpuesto
+     */ 
+    public function getTipoimpuesto()
+    {
+        return $this->tipoimpuesto;
+    }
+
+    /**
+     * Set the value of tipoimpuesto
+     *
+     * @return  self
+     */ 
+    public function setTipoimpuesto($tipoimpuesto)
+    {
+        $this->tipoimpuesto = $tipoimpuesto;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of tipoarticulo
+     */ 
+    public function getTipoarticulo()
+    {
+        return $this->tipoarticulo;
+    }
+
+    /**
+     * Set the value of tipoarticulo
+     *
+     * @return  self
+     */ 
+    public function setTipoarticulo($tipoarticulo)
+    {
+        $this->tipoarticulo = $tipoarticulo;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of idunidadbase
+     */ 
+    public function getIdunidadbase()
+    {
+        return $this->idunidadbase;
+    }
+
+    /**
+     * Set the value of idunidadbase
+     *
+     * @return  self
+     */ 
+    public function setIdunidadbase($idunidadbase)
+    {
+        $this->idunidadbase = $idunidadbase;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of total
+     */ 
+    public function getTotal()
+    {
+        return $this->total;
+    }
+
+    /**
+     * Set the value of total
+     *
+     * @return  self
+     */ 
+    public function setTotal($total)
+    {
+        $this->total = $total;
+
+        return $this;
+    }
 }
 
 ?>
