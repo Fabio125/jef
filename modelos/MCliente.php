616e8e7291a9b14c6f15084854902e5e9c05af03
Farch <farch12@gmail.com>
2019-06-18 01:21:44 -0500

    Cliente Api Sunat y Dni

--- modelos/MCliente.php
+++ modelos/MCliente.php
@@ -33,7 +33,7 @@ class MCliente{
                         $this->dato[1],
                         $this->dato[3],
                         $this->dato[2],
-                        $this->dato[6]                        
+                        $this->dato[4]                        
                     );
      				     
                 }
@@ -47,5 +47,88 @@ class MCliente{
             return $cliente;  
         }
     }    
+
+    public function registrar_cliente($cliente,$usuario){
+
+        try {                      
+            
+            $this->consulta="CALL GuardaCliente(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,@newid)";                         
+            $this->stmt=$this->conexion->prepare($this->consulta);
+            $this->conexion->beginTransaction();
+            $this->stmt->bindParam(1,$codper);          
+            $this->stmt->bindParam(2,$codlista);
+            $this->stmt->bindParam(3,$codven);
+            $this->stmt->bindParam(4,$dni);
+            $this->stmt->bindParam(5,$nombre);
+            $this->stmt->bindParam(6,$ruc);
+            $this->stmt->bindParam(7,$razonsocial);
+            $this->stmt->bindParam(8,$direccionlegal);
+            $this->stmt->bindParam(9,$direccionentrega);
+            $this->stmt->bindParam(10,$telefono);
+            $this->stmt->bindParam(11,$email);
+            $this->stmt->bindParam(12,$web);
+            $this->stmt->bindParam(13,$pais);
+            $this->stmt->bindParam(14,$departamento);
+            $this->stmt->bindParam(15,$provincia);
+            $this->stmt->bindParam(16,$distrito);
+            $this->stmt->bindParam(17,$zona);
+            $this->stmt->bindParam(18,$descuento);
+            $this->stmt->bindParam(19,$formapago);
+            $this->stmt->bindParam(20,$moneda);
+            $this->stmt->bindParam(21,$lineacredito);
+            $this->stmt->bindParam(22,$lineacreditodisponible);
+            $this->stmt->bindParam(23,$comentario);
+            $this->stmt->bindParam(24,$banco);
+            $this->stmt->bindParam(25,$ctacte);
+            $this->stmt->bindParam(26,$contacto);
+            $this->stmt->bindParam(27,$telefonocontacto);
+            $this->stmt->bindParam(28,$calificacion);
+            $this->stmt->bindParam(29,$codusu);
+            $this->stmt->bindParam(30,$tasa);
+            $this->stmt->bindParam(31,$facven);                                   
+            
+            $codper=null;               
+            $codlista=null;     
+            $codven=null;  
+            $dni=$cliente->getDni();  
+            $nombre=$cliente->getNombre();  
+            $ruc=$cliente->getRuc();  
+            $razonsocial=$cliente->getRazonsocial(); 
+            $direccionlegal=$cliente->getDireccionlegal();               
+            $direccionentrega=null;     
+            $telefono=null;  
+            $email=null;  
+            $web=null;  
+            $pais=null;  
+            $departamento=null;
+            $provincia=null;               
+            $distrito=null;     
+            $zona=0;  
+            $descuento=0;  
+            $formapago=6;  
+            $moneda=1;  
+            $lineacredito=0.00;
+            $lineacreditodisponible=0.00;               
+            $comentario=null;     
+            $banco=null;  
+            $ctacte=null;  
+            $contacto=null;  
+            $telefonocontacto=null;  
+            $calificacion=0; 
+            $codusu=$usuario->getIdusuario(); 
+            $tasa=0.00; 
+            $facven=0;            
+            $this->stmt->execute();   
+            $this->conexion->commit();                              
+            return true;
+        
+        } catch (exception $e) {                    
+            
+            $this->conexion->rollback(); 
+            echo "Error en Operacion: " . $e->getMessage();     
+            return false; 
+        }
+
+    }
 }
 ?>
--- vistas/listarclientepordocumento.php
+++ vistas/listarclientepordocumento.php
@@ -1,14 +1,19 @@
 <?php
-
 header('Content-Type: text/plain');
-
 require ("../src/autoload.php");    
+include("../clases/Usuario.php");
 include("../clases/Cliente.php");
 include("../controladores/CCliente.php");
+include("../src/simple_html_dom.php");
 
+session_start();
+
+$usuario=$_SESSION["usuario"];
+$correcto=null;
 $empresa=null;
-$ndocumento="10454729485";//$_POST["documento"];
-$arreglo=array();
+$cliente=null;
+$ndocumento="45472948";//$_POST["documento"];
+$arreglo=null;
 $ccliente=new CCliente();
 $cliente=$ccliente->buscar_clientexndocumento($ndocumento);
 
@@ -21,22 +26,83 @@ if($cliente->getIdcliente()==null){
 
         if($busqueda->success){
 
-            
-        }
-        print_r($arreglo_busqueda);
+            $cliente=new Cliente(
+               null,
+               $busqueda->result->razon_social,
+               '',
+               $busqueda->result->razon_social,
+               $busqueda->result->ruc,
+               $busqueda->result->direccion
+            );    
+            $correcto=$ccliente->registrar_cliente($cliente, $usuario);
+
+            if($correcto){
+
+                $cliente=$ccliente->buscar_clientexndocumento($ndocumento);
+                $arreglo=array();
+                $arreglo[] = [         
+                    "codcliente"=>$cliente->getIdcliente(),        
+                    "nombre"=>$cliente->getNombre(), 
+                    "dni"=>$cliente->getDni(), 
+                    "razonsocial"=>$cliente->getRazonsocial(), 
+                    "ruc"=>$cliente->getRuc(),
+                    "direccion"=>$cliente-> getDireccionlegal()
+                ];
+            }
+        }      
+    }
+    else if(strlen($ndocumento)==8){
+
+        $servicio = @file_get_html('http://aplicaciones007.jne.gob.pe/srop_publico/Consulta/Afiliado/GetNombresCiudadano?DNI='.$ndocumento)->plaintext;
+
+        if($servicio!=null){
+
+            if(strpos($servicio,"|")){
+
+                $datos = explode("|", $servicio);
+
+                $cliente=new Cliente(
+                    null,
+                    $datos[2]." ".$datos[0]." ".$datos[1],
+                    $ndocumento,
+                    $datos[2]." ".$datos[0]." ".$datos[1],
+                    '',
+                    '-'
+                 );    
+                 
+                 $correcto=$ccliente->registrar_cliente($cliente, $usuario);
+
+                if($correcto){
+
+                    $cliente=$ccliente->buscar_clientexndocumento($ndocumento);
+                    $arreglo=array();
+                    $arreglo[] = [         
+                        "codcliente"=>$cliente->getIdcliente(),        
+                        "nombre"=>$cliente->getNombre(), 
+                        "dni"=>$cliente->getDni(), 
+                        "razonsocial"=>$cliente->getRazonsocial(), 
+                        "ruc"=>$cliente->getRuc(),
+                        "direccion"=>$cliente-> getDireccionlegal()
+                    ];
+                }
+            }
+        }       
     }
 
 }else{
 
+    $arreglo=array();
+    $arreglo[] = [         
+        "codcliente"=>$cliente->getIdcliente(),        
+        "nombre"=>$cliente->getNombre(), 
+        "dni"=>$cliente->getDni(), 
+        "razonsocial"=>$cliente->getRazonsocial(), 
+        "ruc"=>$cliente->getRuc(),
+        "direccion"=>$cliente-> getDireccionlegal()
+    ];
 }
 
-/*foreach ($arreglo_usuario as $usuario)
-{
-    $arreglo[] = [         
-        "cuenta"=>$usuario->getCuenta(),            
-    ];
-}*/
+print_r($arreglo);
  //echo json_encode($arreglo);
-
 
 ?>
